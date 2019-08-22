19e9baab85efa96d8c18a049f76d4de256d46b38
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-08-22 19:59:54 +0100

    fiexed bugs

---
+++ components/contact.js
@@ -0,0 +1,21 @@
+
+var Contact = React.createClass({
+    propTypes: {
+        item: React.PropTypes.object.isRequired,
+    },
+
+    render: function () {
+        return (
+            React.createElement('div', { className: 'contactItem' },
+                React.createElement('img', {
+                    className: 'contactImage',
+                    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg4cBkcMmCNp7oItNfYyvf2oTFxi-giAxg99defYuvj7iz41ea'
+                }),
+                React.createElement('p', { className: 'contactLabel labelName' }, 'Name: ' + this.props.item.firstName),
+                React.createElement('p', { className: 'contactLabel labelSurname' }, 'Surname: ' + this.props.item.lastName),
+                React.createElement('a', { className: 'contactEmail'}, 'mailto: ' + this.props.item.email)
+            )
+        )
+
+    },
+});
